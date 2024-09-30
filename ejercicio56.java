d7d92b65a5111eaa6887199b204ec5b9833766b8
emariles <eamariles@estudiante.uniajc.edu.co>
2024-09-30 20:36:31 +0000

    finalizacion ejercicio 56

--- ejercicio56.java
+++ ejercicio56.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+
 public class ejercicio56 {
     public static void main (String []args){
+        int estudiantes =leer.nextInt();
+        double nota = 0.0;
+        int aprobados = 0, noAprobados =0;
 
-        System.out.println("Mi primer programa en JAVA en Github");
+        //notas de los estudiantes
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Digite la cantidadde estudiantes");
+        
+
+        for(int i = 1; i <= estudiantes; i++){
+            System.out.print("Nota delestudiante"+ i + ":");
+            nota = leer.nextDouble();
+         if (nota >= 5.0){
+            aprobados++;
+         }else{
+            noAprobados++;
+         }
+
+     }
+     System.out.println("Aprobados: " + aprobados + "No Aprobados" + noAprobados);
+
+        }
+
 
     }
-}
