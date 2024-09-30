f06f12715932cfbd1b244ae8a55b29515eb52d7e
emariles <eamariles@estudiante.uniajc.edu.co>
2024-09-30 20:49:00 +0000

    finalizacion ejercicio 56

--- ejercicio56.java
+++ ejercicio56.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class ejercicio56 {
     public static void main (String []args){
-        int estudiantes =leer.nextInt();
+      
+      
         double nota = 0.0;
         int aprobados = 0, noAprobados =0;
 
         //notas de los estudiantes
         Scanner leer = new Scanner(System.in);
         System.out.println("Digite la cantidadde estudiantes");
-        
+        int estudiantes = leer.nextInt();
 
         for(int i = 1; i <= estudiantes; i++){
             System.out.print("Nota delestudiante"+ i + ":");
