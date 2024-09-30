044b54a52151a727fa8caa171e0a16f595cbf179
emariles <eamariles@estudiante.uniajc.edu.co>
2024-09-30 21:27:30 +0000

    explicacion Do while

---
+++ ejercicioDowhile.java
@@ -0,0 +1,29 @@
+public class ejercicioDowhile {
+    public static void main(String[] args) {
+        /*
+         * do while
+         */
+        String cad = "programacion I";
+        int j = 1;
+
+        do {
+            System.out.println(cad);
+            j++;
+        } while (j > 5);
+
+        System.out.println("****************************************************");
+        /*
+         * while / for
+         */
+        int i = 1;
+        while (i > 5) {
+            System.out.println(cad);
+        }
+
+        System.out.println("****************************************************");
+
+        for (int k = 1; i < 5; i++) {
+            System.out.println(cad);
+        }
+    }
+}
